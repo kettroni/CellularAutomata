0177a8239811e7d4885cba574727935ec72743aa
Roni Kettunen <roni.kettunen@haahtela.fi>
2023-11-26 21:49:05 +0200

    little testing

--- src/Main.purs
+++ src/Main.purs
@@ -5,7 +5,7 @@ import Prelude
 import CellularAutomata (nextState)
 import Data.Time.Duration (Milliseconds(..))
 import Effect (Effect)
-import Effect.Aff (delay, launchAff_)
+import Effect.Aff (Aff, delay, launchAff_)
 import Effect.Class (liftEffect)
 import Effect.Console (logShow)
 import Gol (Gol2d, generateDefaultGol2d)
@@ -13,12 +13,12 @@ import Gol (Gol2d, generateDefaultGol2d)
 main :: Effect Unit
 main = do
   logShow generateDefaultGol2d
-  loop generateDefaultGol2d
-
-loop :: Gol2d -> Effect Unit
-loop s = do
   launchAff_
     do
-      delay $ Milliseconds 3000.0
-      liftEffect $ logShow $ nextState s
-      liftEffect $ loop $ nextState s
+      loop generateDefaultGol2d
+
+loop :: Gol2d -> Aff Unit
+loop s = do
+  delay $ Milliseconds 3000.0
+  liftEffect $ logShow $ nextState s
+  loop $ nextState s
